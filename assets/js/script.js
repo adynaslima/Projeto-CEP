36073d1f864c460ef9cd2cbdc6f7831ddffc1372
Adyna Lima <adynalsl@gmail.com>
2023-05-21 13:21:00 -0300

    :construction:feat: melhorando o código com try e catch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-async function buscaEndereco() {
-    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
-    var consultaCepConvertida = await consultaCep.json();
-    console.log(consultaCepConvertida);
+async function buscaEndereco(cep) {
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
 
+        console.log(consultaCepConvertida);
+        return (consultaCepConvertida);
+    } catch (erro) {
+        console.log(erro)
+    }
 }
 
-buscaEndereco();
+var cep = document.querySelector('#cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
