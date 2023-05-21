296316b3071b8a99a1a4654e301c5f2c5e7421f1
Adyna Lima <adynalsl@gmail.com>
2023-05-21 13:33:12 -0300

    :construction:feat: ativando o preenchimento dos campos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ async function buscaEndereco(cep) {
         if (consultaCepConvertida.erro) {
             throw Error('CEP não existente!');
         }
+        var cidade = document.querySelector('#cidade');
+        var logradouro = document.querySelector('#endereco');
+        var bairro = document.querySelector('#bairro');
+        var estado = document.querySelector('#estado');
+
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        bairro.value = consultaCepConvertida.bairro;
+        estado.value = consultaCepConvertida.uf; 
 
         console.log(consultaCepConvertida);
         return (consultaCepConvertida);
